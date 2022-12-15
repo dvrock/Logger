e0502bc406477ab7d7ff367051e1be9d4ca657d0
usama <syedusamatanveer@gmail.com>
2022-12-15 11:51:04 +0500

    pull

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
 import {React, useEffect, useMemo} from 'react';
-import { NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import Home from './components/Home';
-import {Logger,Init, sendLog} from './components/Logger';
-import Check from "./components/Check"
+import {Logger, Init, sendLog} from './components/Logger';
+import Check from './components/Check';
 import SyncStorage from 'sync-storage';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-
 console.reportErrorsAsExceptions = false;
- 
+
 export default function App() {
   const Stack = createNativeStackNavigator();
-  
+  useEffect(() => {
+    Init('form_submit', 'Jone');
+  }, []);
 
   setInterval(() => {
     console.log('function called');
     sendLog();
-  }, 1000*60);
+  }, 1000 * 60);
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen
+        <Stack.Screen
           name="Check"
           component={Check}
           options={{title: 'Welcome'}}
@@ -30,8 +31,6 @@ export default function App() {
           component={Home}
           options={{title: 'Welcome'}}
         />
-        
-       
       </Stack.Navigator>
     </NavigationContainer>
   );
